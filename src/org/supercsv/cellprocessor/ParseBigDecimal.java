eb8f9631ad6b87abeeb4fe8443de0018116a6526
Kasper Graversen <kbilsted@users.noreply.github.com>
2008-01-20 14:17:36 +0000

    added ParseBigDecimal processor
    git-svn-id: http://svn.code.sf.net/p/supercsv/code/trunk@49 01ba9438-b82c-4373-9247-da971c9e6a59

---
+++ src/org/supercsv/cellprocessor/ParseBigDecimal.java
@@ -0,0 +1,43 @@
+package org.supercsv.cellprocessor;
+
+import java.math.BigDecimal;
+
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.exception.SuperCSVException;
+import org.supercsv.util.CSVContext;
+
+/**
+ * Convert a string to a big decimal. It constructs BigDecimals from strings as recommended by the Javadoc (e.g.
+ * <tt>new BigDecimal(0.1)</tt> yields unpredictable results, while <tt>new BidDecimal("0.1")</tt> yields
+ * predictable results.
+ * 
+ * @author Kasper B. Graversen
+ */
+public class ParseBigDecimal extends CellProcessorAdaptor {
+
+	public ParseBigDecimal() {
+		super();
+	}
+
+	public ParseBigDecimal(final CellProcessor next) {
+		super(next);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Object execute(final Object value, final CSVContext context) throws NumberFormatException {
+		final BigDecimal result;
+		if(value instanceof String) {
+			result = new BigDecimal((String) value);
+		}
+		else {
+			throw new SuperCSVException("Can't convert \"" + value
+					+ "\" to a BigDecimal. Input is not of type String, but of type " + value.getClass().getName(),
+					context);
+		}
+
+		return next.execute(result, context);
+	}
+}
---
+++ test/org/supercsv/cellprocessor/ParseBigDecimalTest.java
@@ -0,0 +1,56 @@
+package org.supercsv.cellprocessor;
+
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.exception.SuperCSVException;
+import org.supercsv.mock.ComparerCellProcessor;
+import org.supercsv.util.CSVContext;
+
+/**
+ * @author Kasper B. Graversen
+ */
+public class ParseBigDecimalTest {
+	private static final CSVContext	CSVCONTEXT	= new CSVContext(0, 0);
+	private static final BigDecimal	VAL2		= new BigDecimal("-43.0");
+	private static final String		VAL2STR		= "-43.0";
+	private static final BigDecimal	VAL1		= new BigDecimal("17.3");
+	private static final String		VAL1_STR_	= "17.3";
+	CellProcessor					cp, ccp;
+
+	@Test(expected = SuperCSVException.class)
+	public void invalid_input() {
+		Assert.assertEquals(cp.execute('C', CSVCONTEXT), 'C');
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		cp = new ParseBigDecimal();
+	}
+
+	@Test
+	public void testChaining() throws Exception {
+		ccp = new ParseBigDecimal(new ComparerCellProcessor(VAL1));
+		Assert.assertEquals("convert possitive ", true, ccp.execute(VAL1_STR_, CSVCONTEXT));
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void testEmptyInput() throws Exception {
+		cp.execute("", CSVCONTEXT);
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void testInValidInput() throws Exception {
+		Assert.assertEquals(cp.execute("hello", CSVCONTEXT), "");
+	}
+
+	@Test
+	public void validInputTest() throws Exception {
+		Assert.assertEquals("convert possitive", VAL1, cp.execute(VAL1_STR_, CSVCONTEXT));
+		Assert.assertEquals("convert negative", VAL2, cp.execute(VAL2STR, CSVCONTEXT));
+	}
+
+}
