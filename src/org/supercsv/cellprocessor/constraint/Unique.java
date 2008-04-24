8a30ec3d2ab296519bc8ba156902483e05232b7c
Kasper Graversen <kbilsted@users.noreply.github.com>
2008-04-24 19:22:40 +0000

    git-svn-id: http://svn.code.sf.net/p/supercsv/code/trunk@74 01ba9438-b82c-4373-9247-da971c9e6a59

--- src/org/supercsv/cellprocessor/constraint/Unique.java
+++ src/org/supercsv/cellprocessor/constraint/Unique.java
@@ -8,41 +8,41 @@ import org.supercsv.exception.SuperCSVException;
 import org.supercsv.util.CSVContext;
 
 /**
- * Ensure that upon processing a CSV file (reading or writing), that values of the column all are unique. Comparison is
- * based upon each elements <tt>hashCode()</tt> method of the objects and lookup takes O(1).
+ * Ensure that upon processing a CSV file (reading or writing), that values of the column are all unique. Comparison is
+ * based upon each elements <tt>hashCode()</tt> method and if a collision, then <tt>equals()</tt> is used. Lookup
+ * takes O(1) and each object queried for uniqueness is stored in memory.
  * 
  * @author Kasper B. Graversen
  */
 public class Unique extends CellProcessorAdaptor {
-	final static Object					tokenForMap	= new Object();
-	protected HashMap<Integer, Object>	uniqueMap	= new HashMap<Integer, Object>();
+final static Object tokenForMap = new Object();
+protected HashMap<Integer, Object> uniqueMap = new HashMap<Integer, Object>();
 
-	public Unique() {
-		super();
-	}
+public Unique() {
+	super();
+}
 
-	public Unique(final CellProcessor next) {
-		super(next);
-	}
+public Unique(final CellProcessor next) {
+	super(next);
+}
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @throws SuperCSVException
-	 *             upon detecting a duplicate entry
-	 * @return the argument value if the value is unique
-	 */
-	@Override
-	public Object execute(final Object value, final CSVContext context) throws SuperCSVException {
-		// check for uniqueness
-		final int hash = value.hashCode();
-		if(uniqueMap.containsKey(hash))
-			throw new SuperCSVException("Duplicate entry \"" + value + "\" found!");
-
-		// if not found add it
-		uniqueMap.put(hash, tokenForMap);
-
-		// chaining
-		return next.execute(value, context);
-	}
+/**
+ * {@inheritDoc}
+ * 
+ * @throws SuperCSVException
+ *             upon detecting a duplicate entry
+ * @return the argument value if the value is unique
+ */
+@Override
+public Object execute(final Object value, final CSVContext context) throws SuperCSVException {
+	// check for uniqueness
+	final int hash = value.hashCode();
+	if( uniqueMap.containsKey(hash) ) { throw new SuperCSVException("Duplicate entry \"" + value + "\" found!"); }
+	
+	// if not found add it
+	uniqueMap.put(hash, tokenForMap);
+	
+	// chaining
+	return next.execute(value, context);
+}
 }
