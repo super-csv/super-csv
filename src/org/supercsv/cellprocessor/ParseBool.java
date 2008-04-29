d3de25ea5cd1d5269886b213cd2ec5b569846a7b
Dominique De Vito <ddv36a78@users.sf.net>
2008-04-29 19:42:54 +0000

    code refactoring for parsing input value + give the possibility to define specific true/false values.
    git-svn-id: http://svn.code.sf.net/p/supercsv/code/branches/DDV_BRANCH_1@90 01ba9438-b82c-4373-9247-da971c9e6a59

--- src/org/supercsv/cellprocessor/ParseBool.java
+++ src/org/supercsv/cellprocessor/ParseBool.java
@@ -1,46 +1,101 @@
 package org.supercsv.cellprocessor;
 
 import org.supercsv.cellprocessor.ift.BoolCellProcessor;
+import org.supercsv.exception.NullInputException;
 import org.supercsv.exception.SuperCSVException;
 import org.supercsv.util.CSVContext;
 
 /**
- * Convert a string to a boolean. The strings "true", "false", "0", "1", "N", "Y", "F", "T" are the only accepted
- * strings
+ * Convert a string to a boolean while ignoring case. 
+ * The strings "true", "false", "0", "1", "N", "Y", "F", "T" are 
+ * the default accepted strings.
  * 
  * @author Kasper B. Graversen
+ * @author Dominique De Vito
  * @since 1.0
  */
 public class ParseBool extends CellProcessorAdaptor {
 
-public ParseBool() {
-	super();
-}
+	private static String[] DEFAULT_TRUE_VALUES = new String[] {
+		"1",
+		"true",
+		"t",
+		"y"
+	};
 
-public ParseBool(final BoolCellProcessor next) {
-	super(next);
-}
+	private static String[] DEFAULT_FALSE_VALUES = new String[] {
+		"0",
+		"false",
+		"f",
+		"n"
+	};
+	
+	private String[] trueValues;
+	private String[] falseValues;
 
-/**
- * {@inheritDoc}
- * 
- * @throws SuperCSVException
- *             when the value is some value that cannot be translated into a boolean value
- */
-@Override
-public Object execute(final Object value, final CSVContext context) throws SuperCSVException {
-	Boolean result;
-	final String sval = ((String) value).toLowerCase();
-	if( sval.equals("0") || sval.equalsIgnoreCase("false") || sval.equalsIgnoreCase("f") || sval.equalsIgnoreCase("n") ) {
-		result = Boolean.FALSE;
-	} else if( sval.equals("1") || sval.equalsIgnoreCase("true") || sval.equalsIgnoreCase("t")
-		|| sval.equalsIgnoreCase("y") ) {
-		result = Boolean.TRUE;
-	} else {
-		throw new SuperCSVException("Cannot parse \"" + value + "\" to a boolean on line " + context.lineNumber
-			+ " column " + context.columnNumber, context);
+	public ParseBool() {
+		this(DEFAULT_TRUE_VALUES, DEFAULT_FALSE_VALUES);
+	}
+
+	public ParseBool(final BoolCellProcessor next) {
+		this(DEFAULT_TRUE_VALUES, DEFAULT_FALSE_VALUES, next);
+	}
+
+	public ParseBool(final String[] trueValues, final String[] falseValues) {
+		super();
+		this.trueValues = trueValues;
+		this.falseValues = falseValues;
+	}
+
+	public ParseBool(final String[] trueValues, final String[] falseValues, final BoolCellProcessor next) {
+		super(next);
+		this.trueValues = trueValues;
+		this.falseValues = falseValues;
+	}
+
+	
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @throws SuperCSVException
+	 *             when the value is some value that cannot be translated into a boolean value
+	 */
+	@Override
+	public Object execute(final Object value, final CSVContext context) throws SuperCSVException {
+		if( value == null ) { throw new NullInputException("Input cannot be null", context, this); }
+		Boolean result;
+		final String sval = ((String) value).toLowerCase();
+		if(isFalseValue(sval)) {
+			result = Boolean.FALSE;
+		}
+		else if(isTrueValue(sval)) {
+			result = Boolean.TRUE;
+		}
+		else {
+			throw new SuperCSVException("Cannot parse \"" + value + "\" to a boolean", 
+					context, this);
+		}
+
+		return next.execute(result, context);
+	}
+
+	private boolean isTrueValue(final String sval) {
+		return indexOf(sval, trueValues) >= 0;
+	}
+
+	private boolean isFalseValue(final String sval) {
+		return indexOf(sval, falseValues) >= 0;
 	}
 	
-	return next.execute(result, context);
-}
+	private static int indexOf(final String sval, final String[] possibleMatches) {
+		if (possibleMatches == null) {
+			return -1;
+		}
+		for (int i = 0; i < possibleMatches.length; i++) {
+			if (sval.equals(possibleMatches[i])) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
