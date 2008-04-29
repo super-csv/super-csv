38165a4f001d26271924f6b1b77b4c476a3f2a11
Dominique De Vito <ddv36a78@users.sf.net>
2008-04-29 20:08:46 +0000

    add of the input value check into the execute() method + this class implements now all CellProcessor as it can accept (ans was accepting before) all kind of values as input.
    git-svn-id: http://svn.code.sf.net/p/supercsv/code/branches/DDV_BRANCH_1@94 01ba9438-b82c-4373-9247-da971c9e6a59

--- src/org/supercsv/cellprocessor/StrReplace.java
+++ src/org/supercsv/cellprocessor/StrReplace.java
@@ -1,12 +1,31 @@
 package org.supercsv.cellprocessor;
 
+import org.supercsv.cellprocessor.ift.BoolCellProcessor;
+import org.supercsv.cellprocessor.ift.DateCellProcessor;
+import org.supercsv.cellprocessor.ift.DoubleCellProcessor;
+import org.supercsv.cellprocessor.ift.LongCellProcessor;
 import org.supercsv.cellprocessor.ift.StringCellProcessor;
+import org.supercsv.exception.NullInputException;
 import org.supercsv.exception.SuperCSVException;
 import org.supercsv.util.CSVContext;
 
-public class StrReplace extends CellProcessorAdaptor {
+/**
+ * String replacer.
+ * @author Kasper B. Graversen
+ * @author Dominique De Vito (ddv36a78@yahoo.fr)
+ */
+public class StrReplace extends CellProcessorAdaptor implements BoolCellProcessor,
+DateCellProcessor, DoubleCellProcessor, LongCellProcessor, StringCellProcessor {
 private String searchText, replaceText;
 
+/**
+ * String replace
+ * 
+ * @param searchText
+ *            text to search for
+ * @param replaceText
+ *            text to replace with
+ */
 public StrReplace(final String searchText, final String replaceText) {
 	super();
 	handleArguments(searchText, replaceText);
@@ -21,15 +40,18 @@ public StrReplace(final String searchText, final String replaceText, final Strin
  * {@inheritDoc}
  */
 @Override
-public Object execute(final Object value, final CSVContext context) throws SuperCSVException {
+public Object execute(final Object value, final CSVContext context) {
+	if (value == null) {
+		throw new NullInputException("Input cannot be null", context, this); 
+	}
 	return value.toString().replaceAll(searchText, replaceText);
 }
 
 private void handleArguments(final String searchText, final String replaceText) throws IllegalArgumentException {
-	if( searchText == null ) { throw new SuperCSVException("argument searchText cannot be null"); }
-	if( replaceText == null ) { throw new SuperCSVException("argument replaceText cannot be null"); }
+	if( searchText == null ) { throw new NullInputException("searchtext cannot be null", this); }
+	if( replaceText == null ) { throw new NullInputException("replacettext cannot be null", this); }
 	if( searchText.equals("") ) { throw new SuperCSVException(
-		"argument searchText cannot be \"\" as this has no effect"); }
+		"argument searchText cannot be \"\" as this has no effect", this); }
 	this.searchText = searchText;
 	this.replaceText = replaceText;
 }
