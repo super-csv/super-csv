0c63abebe73a356edb1f83a39fe5dd898f65994c
Kasper Graversen <kbilsted@users.noreply.github.com>
2008-05-08 22:52:55 +0000

    m reformated
    git-svn-id: http://svn.code.sf.net/p/supercsv/code/trunk@107 01ba9438-b82c-4373-9247-da971c9e6a59

--- src/org/supercsv/cellprocessor/constraint/Strlen.java
+++ src/org/supercsv/cellprocessor/constraint/Strlen.java
@@ -14,11 +14,11 @@ import org.supercsv.util.CSVContext;
  * The length constraints must all be > 0 or an exception is thrown Lookup time is O(1).
  * 
  * @author Kasper B. Graversen
- *  @author Dominique De Vito
+ * @author Dominique De Vito
  */
 public class Strlen extends CellProcessorAdaptor implements StringCellProcessor {
 /** Set of all accepted lengths */
-protected HashSet<Integer>	requiredLengths	= new HashSet<Integer>();
+protected HashSet<Integer> requiredLengths = new HashSet<Integer>();
 
 public Strlen(final int... requiredLengths) {
 	super();
@@ -53,26 +53,23 @@ protected void addValues(final int... requiredLengths) throws SuperCSVException 
  */
 @Override
 public Object execute(final Object value, final CSVContext context) throws SuperCSVException, ClassCastException {
-	if( value == null ) { throw new NullInputException("Input cannot be null on line " + context.lineNumber + " at column " + context.columnNumber, context, this); }
+	if( value == null ) { throw new NullInputException("Input cannot be null on line " + context.lineNumber
+		+ " at column " + context.columnNumber, context, this); }
 	final String sval = (value == null) ? null : value.toString(); // cast
 	final int slength = (sval == null) ? 0 : sval.length();
-		
-
+	
 	// check for required lengths
-	if(!requiredLengths.contains(slength)) {
-				throw new SuperCSVException("Entry \"" + value + "\" is not of any of " +
-			"the required lengths " + printRequiredLengths(), context,this);
-	}
+	if( !requiredLengths.contains(slength) ) { throw new SuperCSVException("Entry \"" + value + "\" is not of any of "
+		+ "the required lengths " + printRequiredLengths(), context, this); }
 	
 	return next.execute(value, context);
 }
 
 private String printRequiredLengths() {
-	// create string of required lengths
 	final StringBuilder sb = new StringBuilder();
 	String currentSeparator = "";
 	String separator = ", ";
-	for(final int length : requiredLengths) {
+	for( final int length : requiredLengths ) {
 		sb.append(currentSeparator);
 		sb.append(length);
 		currentSeparator = separator;
