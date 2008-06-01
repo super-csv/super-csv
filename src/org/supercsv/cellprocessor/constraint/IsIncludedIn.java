f05f696e85fb8226ce15ae3a6b258686512463d8
Dominique De Vito <ddv36a78@users.sf.net>
2008-06-01 04:44:13 +0000

    intro
    git-svn-id: http://svn.code.sf.net/p/supercsv/code/trunk@131 01ba9438-b82c-4373-9247-da971c9e6a59

---
+++ src/org/supercsv/cellprocessor/constraint/IsIncludedIn.java
@@ -0,0 +1,81 @@
+package org.supercsv.cellprocessor.constraint;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.supercsv.cellprocessor.CellProcessorAdaptor;
+import org.supercsv.cellprocessor.ift.BoolCellProcessor;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.cellprocessor.ift.DateCellProcessor;
+import org.supercsv.cellprocessor.ift.DoubleCellProcessor;
+import org.supercsv.cellprocessor.ift.LongCellProcessor;
+import org.supercsv.cellprocessor.ift.StringCellProcessor;
+import org.supercsv.exception.NullInputException;
+import org.supercsv.exception.SuperCSVException;
+import org.supercsv.util.CSVContext;
+
+/**
+ * This processor enforces the input value to belong to a specific set of given values.
+ * <p>
+ * 
+ * @since 1.50
+ * @author Dominique De Vito
+ */
+public class IsIncludedIn extends CellProcessorAdaptor implements BoolCellProcessor, DateCellProcessor,
+	DoubleCellProcessor, LongCellProcessor, StringCellProcessor {
+protected Set<Object> possibleValues;
+
+public IsIncludedIn(final HashSet<Object> possibleValues) {
+	super();
+	this.possibleValues = possibleValues;
+}
+
+public IsIncludedIn(final HashSet<Object> possibleValues, final CellProcessor next) {
+	super(next);
+	this.possibleValues = possibleValues;
+}
+
+public IsIncludedIn(final Object[] possibleValues) {
+	super();
+	this.possibleValues = createSet(possibleValues);
+}
+
+public IsIncludedIn(final Object[] possibleValues, final CellProcessor next) {
+	super(next);
+	this.possibleValues = createSet(possibleValues);
+}
+ 
+private static Set<Object> createSet(Object[] arr) {
+	int nb = (arr == null) ? 0 : arr.length;
+	if (nb == 0) {
+		return new HashSet<Object>();
+	} else {
+		HashSet<Object> set = new HashSet<Object>((4 * nb / 3) + 1);
+		for( int i = 0; i < arr.length; i++ ) {
+			set.add(arr[i]);
+		}
+		return set;
+	}
+}
+/**
+ * {@inheritDoc}
+ * 
+ * @throws SuperCSVException
+ *             upon receiving a string of an un-accepted length
+ * @throws ClassCastException
+ *             is the parameter value cannot be cast to a String
+ * @return the argument value if the value is unique
+ */
+@Override
+public Object execute(final Object value, final CSVContext context) throws SuperCSVException, ClassCastException {
+	if( value == null ) { throw new NullInputException("Input cannot be null on line " + context.lineNumber + " at column " + context.columnNumber, context, this); }
+	// check for required hash
+	if( !possibleValues.contains(value) ) {
+		
+		throw new SuperCSVException("Entry \"" + value + "\" on line " + context.lineNumber + " column "
+			+ context.columnNumber + " is accepted as a possible value", context, this);
+	}
+	
+	return next.execute(value, context);
+}
+}
