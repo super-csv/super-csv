b48c7db1cf38d871512c6d4923ab7d0620cad850
Dominique De Vito <ddv36a78@users.sf.net>
2008-06-03 06:08:38 +0000

    introduction of equals() and hashCode() methods
    git-svn-id: http://svn.code.sf.net/p/supercsv/code/trunk@135 01ba9438-b82c-4373-9247-da971c9e6a59

--- src/org/supercsv/util/CSVContext.java
+++ src/org/supercsv/util/CSVContext.java
@@ -24,4 +24,37 @@ public CSVContext(final int lineNumber, final int columnNumber) {
 public String toString() {
 	return String.format("Line: %d Column: %d Raw line:\n%s\n", lineNumber, columnNumber, lineSource);
 }
+
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + columnNumber;
+	result = prime * result + lineNumber;
+	result = prime * result + ((lineSource == null) ? 0 : lineSource.hashCode());
+	return result;
+}
+
+@Override
+public boolean equals(Object obj) {
+	if( this == obj )
+		return true;
+	if( obj == null )
+		return false;
+	if( getClass() != obj.getClass() )
+		return false;
+	final CSVContext other = (CSVContext) obj;
+	if( columnNumber != other.columnNumber )
+		return false;
+	if( lineNumber != other.lineNumber )
+		return false;
+	if( lineSource == null ) {
+		if( other.lineSource != null )
+			return false;
+	} else if( !lineSource.equals(other.lineSource) )
+		return false;
+	return true;
+}
+
+
 }
