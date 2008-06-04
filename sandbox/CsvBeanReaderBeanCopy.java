e9057572de8257a1655672c619bd535895d78ecd
Kasper Graversen <kbilsted@users.noreply.github.com>
2008-06-04 20:11:20 +0000

    beanreader added..
    git-svn-id: http://svn.code.sf.net/p/supercsv/code/trunk@156 01ba9438-b82c-4373-9247-da971c9e6a59

---
+++ sandbox/CsvBeanReaderBeanCopy.java
@@ -0,0 +1,115 @@
+
+
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.exception.SuperCSVException;
+import org.supercsv.exception.SuperCSVReflectionException;
+import org.supercsv.prefs.CsvPreference;
+import org.supercsv.util.BeanInterfaceProxy;
+import org.supercsv.util.MethodCache;
+import org.supercsv.util.Util;
+
+/**
+ * This class reads a line from a csv file, instantiates a bean and populate its fields.
+ * 
+ * @author Kasper B. Graversen
+ */
+public class CsvBeanReaderBeanCopy extends AbstractCsvReader implements ICsvBeanReader {
+/**
+ * object used for storing intermediate result of a processing of cell processors and before put into maps/objects etc..
+ * due to the typing, we cannot use super.line
+ */
+protected List<? super Object> lineResult = new ArrayList<Object>();
+protected MethodCache cache = new MethodCache();
+
+/**
+ * Create a csv reader with a specific preference. Note that the <tt>reader</tt> provided in the argument will be
+ * wrapped in a <tt>BufferedReader</tt> before accessed.
+ */
+public CsvBeanReaderBeanCopy(final Reader reader, final CsvPreference preferences) {
+	setPreferences(preferences);
+	setInput(reader);
+}
+
+/**
+ * Creates an object of the type or if it is an interface, create a proxy instance implementing the interface type.
+ * 
+ * @param clazz
+ *            the type to instantiate. If the type is a class type, an instance can be created straight away. If the
+ *            type is an interface type, a proxy is created on the fly which acts as an implementation.
+ * @param nameMapping
+ * @return A filled object
+ * @throws InstantiationException
+ * @throws IllegalAccessException
+ * @throws InvocationTargetException
+ */
+<T> T fillObject(final Class<T> clazz, final String[] nameMapping) throws SuperCSVReflectionException {
+	try {
+		// create a proxy instance if an interface type is provided
+		final T resultBean;
+		if( clazz.isInterface() ) {
+			resultBean = (T) new BeanInterfaceProxy().createProxy(clazz);
+		}
+		else {
+			resultBean = clazz.newInstance();
+		}
+		// map results into an object by traversing the list of nameMapping and for each non-null,
+		// map that name to an entry in the lineResult
+		// map results to the setter methods
+		for( int i = 0; i < nameMapping.length; i++ ) {
+			// don't call a set-method in the bean, if there is no result to store
+			if( nameMapping[i] == null ) {
+				continue;
+			}
+			try {
+				BeanUtils.setProperty(resultBean, nameMapping[i], lineResult.get(i));
+			}
+			catch(final IllegalArgumentException e) {
+				throw new SuperCSVException("Method set" + nameMapping[i].substring(0, 1).toUpperCase()
+					+ nameMapping[i].substring(1) + "() does not accept input \"" + lineResult.get(i) + "\" of type "
+					+ lineResult.get(i).getClass().getName(), null, e);
+			}
+		}
+		return resultBean;
+	}
+	catch(final InstantiationException e) {
+		throw new SuperCSVReflectionException("Error while filling an object", e);
+	}
+	catch(final IllegalAccessException e) {
+		throw new SuperCSVReflectionException("Error while filling an object", e);
+	}
+	catch(final InvocationTargetException e) {
+		throw new SuperCSVReflectionException("Error while filling an object", e);
+	}
+}
+
+/**
+ * {@inheritDoc}
+ */
+public <T> T read(final Class<T> clazz, final String... nameMapping) throws IOException, SuperCSVReflectionException {
+	if( tokenizer.readStringList(super.line) ) {
+		lineResult.clear();
+		lineResult.addAll(super.line);
+		return fillObject(clazz, nameMapping);
+	}
+	return null; // EOF
+}
+
+/**
+ * {@inheritDoc}
+ */
+public <T> T read(final Class<T> clazz, final String[] nameMapping, final CellProcessor[] processors)
+	throws IOException, SuperCSVReflectionException, SuperCSVException {
+	if( tokenizer.readStringList(super.line) ) {
+		Util.processStringList(lineResult, super.line, processors, tokenizer.getLineNumber());
+		return fillObject(clazz, nameMapping);
+	}
+	return null; // EOF
+}
+}
---
+++ sandbox/ReadingSpeedTest.java
@@ -0,0 +1,219 @@
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.Date;
+
+import org.junit.Test;
+import org.supercsv.io.CsvBeanReader;
+import org.supercsv.io.CsvBeanReaderBeanCopy;
+import org.supercsv.io.CsvListReader;
+import org.supercsv.io.CsvMapReader;
+import org.supercsv.io.Tokenizer_v102;
+import org.supercsv.prefs.CsvPreference;
+
+/**
+ * Read speed-test the CSV against native String.split()
+ * 
+ * @author Kasper B. Graversen
+ */
+public class ReadingSpeedTest extends AbstractSpeedTest {
+public static final class RealBean {
+
+String name1, name2;
+long no, phone;
+Date date;
+
+public Date getDate() {
+	return date;
+}
+
+public String getName1() {
+	return name1;
+}
+
+public String getName2() {
+	return name2;
+}
+
+public long getNo() {
+	return no;
+}
+
+public long getPhone() {
+	return phone;
+}
+
+public void setDate(final Date date) {
+	this.date = date;
+}
+
+public void setName1(final String name1) {
+	this.name1 = name1;
+}
+
+public void setName2(final String name2) {
+	this.name2 = name2;
+}
+
+public void setNo(final long no) {
+	this.no = no;
+}
+
+public void setPhone(final long phone) {
+	this.phone = phone;
+}
+}
+
+public static final class StringsBean {
+String no, name1, name2, phone, date;
+
+public String getDate() {
+	return date;
+}
+
+public String getName1() {
+	return name1;
+}
+
+public String getName2() {
+	return name2;
+}
+
+public String getNo() {
+	return no;
+}
+
+public String getPhone() {
+	return phone;
+}
+
+public void setDate(final String date) {
+	this.date = date;
+}
+
+public void setName1(final String name1) {
+	this.name1 = name1;
+}
+
+public void setName2(final String name2) {
+	this.name2 = name2;
+}
+
+public void setNo(final String no) {
+	this.no = no;
+}
+
+public void setPhone(final String phone) {
+	this.phone = phone;
+}
+}
+
+private void readCsvBeanFullBeanUtilsReader(final Reader r) throws Exception {
+	final CsvBeanReaderBeanCopy csv = new CsvBeanReaderBeanCopy(r, CsvPreference.EXCEL_PREFERENCE);
+	while( csv.read(RealBean.class, header, processors) != null ) {
+		// System.out.print("/");
+	}
+	r.close();
+}
+
+private void readCsvBeanFullReader(final Reader r) throws Exception {
+	final CsvBeanReader csv = new CsvBeanReader(r, CsvPreference.EXCEL_PREFERENCE);
+	while( csv.read(RealBean.class, header, processors) != null ) {
+		// System.out.print("/");
+	}
+	r.close();
+}
+
+private void readCsvBeanReader(final Reader r) throws Exception {
+	final CsvBeanReader csv = new CsvBeanReader(r, CsvPreference.EXCEL_PREFERENCE);
+	while( csv.read(StringsBean.class, header) != null ) {
+		// System.out.print("/");
+	}
+	r.close();
+}
+
+private void readCsvListReader(final Reader r) throws Exception {
+	final CsvListReader csv = new CsvListReader(r, CsvPreference.EXCEL_PREFERENCE);
+	while( csv.read() != null ) {
+		// System.out.print("*");
+	}
+	r.close();
+}
+
+private void readCsvListReaderOldTokenizer(final Reader r) throws Exception {
+	final CsvListReader csv = new CsvListReader(r, CsvPreference.EXCEL_PREFERENCE);
+	csv.setTokenizer(new Tokenizer_v102(r, CsvPreference.EXCEL_PREFERENCE));
+	while( csv.read() != null ) {
+		// System.out.print("*");
+	}
+	r.close();
+}
+
+private void readCsvMapReader(final Reader r) throws Exception {
+	final CsvMapReader csv = new CsvMapReader(r, CsvPreference.EXCEL_PREFERENCE);
+	while( csv.read(header) != null ) {
+		// System.out.print(";");
+	}
+	r.close();
+}
+
+private void readNative(final BufferedReader r) throws Exception {
+	String line;
+	while( (line = r.readLine()) != null ) {
+		// System.out.print(".");
+	}
+	r.close();
+}
+
+@Test
+public void readSpeedTest() throws Exception {
+	final long[] times = new long[20];
+	for( int i = 0; i < TEST_RUNS; i++ ) {
+		int testNo = 0;
+		System.out.println("readSpeedTest() " + i);
+		long t;
+		t = System.currentTimeMillis();
+		readNative(new BufferedReader(new FileReader(TEST_FILE)));
+		times[testNo++] += System.currentTimeMillis() - t;
+		
+		t = System.currentTimeMillis();
+		readCsvListReader(new FileReader(TEST_FILE));
+		times[testNo++] += System.currentTimeMillis() - t;
+		
+		t = System.currentTimeMillis();
+		readCsvListReaderOldTokenizer(new FileReader(TEST_FILE));
+		times[testNo++] += System.currentTimeMillis() - t;
+		
+		// time only simple string
+		t = System.currentTimeMillis();
+		readCsvMapReader(new FileReader(TEST_FILE));
+		times[testNo++] += System.currentTimeMillis() - t;
+		
+		t = System.currentTimeMillis();
+		readCsvBeanReader(new FileReader(TEST_FILE));
+		times[testNo++] += System.currentTimeMillis() - t;
+		
+		t = System.currentTimeMillis();
+		readCsvBeanFullReader(new FileReader(TEST_FILE));
+		times[testNo++] += System.currentTimeMillis() - t;
+		
+		t = System.currentTimeMillis();
+		readCsvBeanFullBeanUtilsReader(new FileReader(TEST_FILE));
+		times[testNo++] += System.currentTimeMillis() - t;
+	}
+	{
+		int i = 0;
+		System.out.println("<table><tr><th>Method</th><th>Relative speed</th><th>Average execution time</th></tr>");
+		System.out.println(makeTableLine("split()", times[0], times[i++]));
+		System.out.println(makeTableLine("ListReader", times[0], times[i++]));
+		System.out.println(makeTableLine("ListReader_old tokenizer", times[0], times[i++]));
+		System.out.println(makeTableLine("MapReader", times[0], times[i++]));
+		System.out.println(makeTableLine("BeanReader", times[0], times[i++]));
+		System.out.println(makeTableLine("BeanReader (full)", times[0], times[i++]));
+		System.out.println(makeTableLine("BeanReaderBeanUtil (full)", times[0], times[i++]));
+		System.out.println("</table>");
+	}
+}
+}
