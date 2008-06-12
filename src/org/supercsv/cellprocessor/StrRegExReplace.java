2260ecce13b08b1c50a4cd03cc5eb3ca584dea31
Dominique De Vito <ddv36a78@users.sf.net>
2008-06-12 07:08:04 +0000

    better checks for constructor arguments
    git-svn-id: http://svn.code.sf.net/p/supercsv/code/trunk@171 01ba9438-b82c-4373-9247-da971c9e6a59

--- src/org/supercsv/cellprocessor/StrRegExReplace.java
+++ src/org/supercsv/cellprocessor/StrRegExReplace.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 import org.supercsv.cellprocessor.ift.BoolCellProcessor;
 import org.supercsv.cellprocessor.ift.StringCellProcessor;
 import org.supercsv.exception.NullInputException;
+import org.supercsv.exception.SuperCSVException;
 import org.supercsv.util.CSVContext;
 
 /**
@@ -20,18 +21,24 @@ private final String replacement;
 
 public StrRegExReplace(final String regex, final String replacement) {
 	super();
-	if( regex == null ) { throw new NullInputException("Regex cannot be null", this); }
+	handleArguments(regex, replacement); 
 	this.regexPattern = Pattern.compile(regex);
 	this.replacement = replacement;
 }
 
 public StrRegExReplace(final String regex, final String replacement, final BoolCellProcessor next) {
 	super(next);
-	if( regex == null ) { throw new NullInputException("Regex cannot be null", this); }
+	handleArguments(regex, replacement); 
 	this.regexPattern = Pattern.compile(regex);
 	this.replacement = replacement;
 }
 
+private void handleArguments(final String regex, final String replacement)  {
+	if( regex == null ) { throw new NullInputException("the regular expression cannot be null", this); }
+	if( replacement == null ) { throw new NullInputException("the replacement string cannot be null", this); }
+	if( regex.equals("") ) { throw new SuperCSVException(
+		"the regular expression  cannot be \"\" as this has no effect", this); }
+}
 /**
  * {@inheritDoc}
  */
