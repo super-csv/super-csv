d5ef1c8fb955c65fa2baea5c0459c7b45f0d5527
Dominique De Vito <ddv36a78@users.sf.net>
2008-07-01 17:17:25 +0000

    intro of the method registerMessage(), that is, the capability of explaining in plain language the constraint of a regular expression.
    git-svn-id: http://svn.code.sf.net/p/supercsv/code/trunk@194 01ba9438-b82c-4373-9247-da971c9e6a59

--- src/org/supercsv/cellprocessor/constraint/StrRegEx.java
+++ src/org/supercsv/cellprocessor/constraint/StrRegEx.java
@@ -1,9 +1,9 @@
 package org.supercsv.cellprocessor.constraint;
 
+import java.util.HashMap;
 import java.util.regex.Pattern;
 
 import org.supercsv.cellprocessor.CellProcessorAdaptor;
-import org.supercsv.cellprocessor.ift.BoolCellProcessor;
 import org.supercsv.cellprocessor.ift.StringCellProcessor;
 import org.supercsv.exception.NullInputException;
 import org.supercsv.exception.SuperCSVException;
@@ -20,6 +20,8 @@ public class StrRegEx extends CellProcessorAdaptor implements StringCellProcesso
 
 private final String regex;
 private final Pattern regexPattern;
+
+private static final HashMap<String, String> regexMessages = new HashMap<String, String>();
 
 public StrRegEx(final String regex) {
 	super();
@@ -50,9 +52,32 @@ public Object execute(final Object value, final CSVContext context) {
 	
 	boolean found = regexPattern.matcher((String) value).find();
 	if (!found) {
+		String msg = regexMessages.get(regex);
+		if (msg == null) {
 		throw new SuperCSVException(
 			"Entry \"" + value + "\" does not respect the regular expression '" + regex + "'", context, this);
+		} else {
+			throw new SuperCSVException(
+				"Entry \"" + value + "\" does not respect the constraint '" + msg + "' " +
+						"(defined by the regular expression '" + regex + "')", context, this);			
+		}
 	}
 	return next.execute(value, context);
 }
+
+
+/**
+ * Register a message detailing in plain language
+ * the constraint representing a regular expression.
+ * For example, the regular expression \d{0,6}(\.\d{0,3})?
+ * could be associated with the message "up to 6 digits 
+ * into the natural part, and up to 3 digit into the decimal part".
+ * @param regex
+ * @param message
+ */
+public static void registerMessage(String regex, String message) {
+	regexMessages.put(regex, message);
+}
+
+
 }
