53dca6a30d00955bec6eb3b8ac646c7d16fc4159
Singularity FX <singularityfx.dev@gmail.com>
2015-06-17 18:33:50 +0400

    Extract test data from CsvResultSetWriterTest
    Extract test data arrays from CsvResultSetWriterTest to SuperCsvTestUtils
    in order for it to be reused in other tests. The same test data was used
    during construction of CustomerBeans in SuperCsvTestUtils, therefore
    in order to avoid duplication of test data, CustomerBeans are now created
    from array values.

--- super-csv/src/test/java/org/supercsv/SuperCsvTestUtils.java
+++ super-csv/src/test/java/org/supercsv/SuperCsvTestUtils.java
@@ -16,6 +16,7 @@
 package org.supercsv;
 
 import static org.junit.Assert.assertEquals;
+import static org.supercsv.SuperCsvTestUtils.date;
 
 import java.util.Arrays;
 import java.util.Calendar;
@@ -61,79 +62,238 @@ public class SuperCsvTestUtils {
 	/** the processors to use for CSV writing tests */
 	public static final CellProcessor[] WRITE_PROCESSORS = new CellProcessor[] { null, null, null,
 		new FmtDate("dd/MM/yyyy"), null, new Optional(new FmtBool("Y", "N")), null, null, null, null };
+
+	public static final Object[][] TEST_DATA_VARIOUS_TYPES = {
+		{"1", "John", "Dunbar", date(1945, 6, 13),
+			"1600 Amphitheatre Parkway\nMountain View, CA 94043\nUnited States", null, null,
+			"\"May the Force be with you.\" - Star Wars", "jdunbar@gmail.com", 0L},
+		{"2", "Bob", "Down", date(1919, 2, 25),
+			"1601 Willow Rd.\nMenlo Park, CA 94025\nUnited States", true, 0,
+			"\"Frankly, my dear, I don't give a damn.\" - Gone With The Wind", "bobdown@hotmail.com", 123456L},
+		{"3", "Alice", "Wunderland", date(1985, 8, 8),
+			"One Microsoft Way\nRedmond, WA 98052-6399\nUnited States", true, 0,
+			"\"Play it, Sam. Play \"As Time Goes By.\"\" - Casablanca", "throughthelookingglass@yahoo.com", 2255887799L},
+		{"4", "Bill", "Jobs", date(1973, 7, 10),
+			"2701 San Tomas Expressway\nSanta Clara, CA 95050\nUnited States", true, 3,
+			"\"You've got to ask yourself one question: \"Do I feel lucky?\" Well, do ya, punk?\" - Dirty Harry",
+			"billy34@hotmail.com", 36L},
+		{"5", "Miranda", "Feist", date(1999, 1, 3),
+			"2-4 Rue du Sablon\nMorges, 1110\nSwitzerland", null, null, "\"You had me at \"hello.\"\" - Jerry Maguire",
+			"miranda_feist@gmail.com", 54623L},
+		{"6", "Steve", "Gates", date(2000, 12, 31),
+			"701 First Avenue\nSunnyvale, CA 94089\nUnited States", false, 0,
+			"\"Gentlemen, you can't fight in here! This is the War Room!\" - Dr Strangelove", "stevengates@yahoo.com",
+			1341512L},
+		{"7", "Ada", "Von Trappe", date(1956, 11, 18),
+			"One Dell Way\nRound Rock, TX 78682\nUnited States", null, 2,
+			"\"Hasta la vista, baby.\" - Terminator 2: Judgement Day", "vonada@gmail.com", 0L},
+		{"8", "Sergei", "Denisovich", date(1944, 6, 22),
+			"1 Infinite Loop\nCupertino, CA 95014\nUnited States", true, null,
+			"\"Open the pod bay doors, HAL.\" - 2001: A Space Odyssey", "sergei@denisovich.com", 229431L},
+		{"9", "Larry", "Ballmer", date(1901, 1, 1),
+			"1-7-1 Konan\nMinato-ku\nTokyo, 108-0075\nJapan", false, 0, "\"A martini. Shaken, not stirred.\" - Goldfinger",
+			"lazza99@gmail.com", 164L},
+		{"10", "Grace", "Fowler", date(2003, 11, 28),
+			"11-1, Kamitoba Hokotate-cho\nMinami-ku\nKyoto, 601-8501\nJapan", false, 0,
+			"\"Carpe diem. Seize the day, boys. Make your lives extraordinary.\" - Dead Poets Society",
+			"gracie@hotmail.com", 168841L},
+	};
+		
+	public static final Object[][] TEST_DATA_STRINGS = {
+		{"1", "John", "Dunbar", "13/06/1945",
+			"1600 Amphitheatre Parkway\nMountain View, CA 94043\nUnited States", "", "",
+			"\"May the Force be with you.\" - Star Wars", "jdunbar@gmail.com", "0"},
+		{"2", "Bob", "Down", "25/02/1919",
+			"1601 Willow Rd.\nMenlo Park, CA 94025\nUnited States", "Y", "0",
+			"\"Frankly, my dear, I don't give a damn.\" - Gone With The Wind", "bobdown@hotmail.com", "123456"},
+		{"3", "Alice", "Wunderland", "08/08/1985",
+			"One Microsoft Way\nRedmond, WA 98052-6399\nUnited States", "Y", "0",
+			"\"Play it, Sam. Play \"As Time Goes By.\"\" - Casablanca", "throughthelookingglass@yahoo.com", "2255887799"},
+		{"4", "Bill", "Jobs", "10/07/1973",
+			"2701 San Tomas Expressway\nSanta Clara, CA 95050\nUnited States", "Y", "3",
+			"\"You've got to ask yourself one question: \"Do I feel lucky?\" Well, do ya, punk?\" - Dirty Harry",
+			"billy34@hotmail.com", "36"},
+		{"5", "Miranda", "Feist", "03/01/1999",
+			"2-4 Rue du Sablon\nMorges, 1110\nSwitzerland", "", "", "\"You had me at \"hello.\"\" - Jerry Maguire",
+			"miranda_feist@gmail.com", "54623"},
+		{"6", "Steve", "Gates", "31/12/2000",
+			"701 First Avenue\nSunnyvale, CA 94089\nUnited States", "N", "0",
+			"\"Gentlemen, you can't fight in here! This is the War Room!\" - Dr Strangelove", "stevengates@yahoo.com",
+			"1341512"},
+		{"7", "Ada", "Von Trappe", "18/11/1956",
+			"One Dell Way\nRound Rock, TX 78682\nUnited States", "", "2",
+			"\"Hasta la vista, baby.\" - Terminator 2: Judgement Day", "vonada@gmail.com", "0"},
+		{"8", "Sergei", "Denisovich", "22/06/1944",
+			"1 Infinite Loop\nCupertino, CA 95014\nUnited States", "Y", "",
+			"\"Open the pod bay doors, HAL.\" - 2001: A Space Odyssey", "sergei@denisovich.com", "229431"},
+		{"9", "Larry", "Ballmer", "01/01/1901",
+			"1-7-1 Konan\nMinato-ku\nTokyo, 108-0075\nJapan", "N", "0", "\"A martini. Shaken, not stirred.\" - Goldfinger",
+			"lazza99@gmail.com", "164"},
+		{"10", "Grace", "Fowler", "28/11/2003",
+			"11-1, Kamitoba Hokotate-cho\nMinami-ku\nKyoto, 601-8501\nJapan", "N", "0",
+			"\"Carpe diem. Seize the day, boys. Make your lives extraordinary.\" - Dead Poets Society",
+			"gracie@hotmail.com", "168841"},
+	};
 	
 	// each line in the CSV file and it's corresponding CustomerBean/CustomerStringBeans for testing
 	public static final String JOHN_CSV = "1,John,Dunbar,13/06/1945,\"1600 Amphitheatre Parkway\r\nMountain View, CA 94043\r\nUnited States\","
 		+ ",,\"\"\"May the Force be with you.\"\" - Star Wars\",jdunbar@gmail.com,0";
-	public static final CustomerBean JOHN = new CustomerBean("1", "John", "Dunbar", date(1945, 6, 13),
-		"1600 Amphitheatre Parkway\nMountain View, CA 94043\nUnited States", null, null,
-		"\"May the Force be with you.\" - Star Wars", "jdunbar@gmail.com", 0L);
+	public static final CustomerBean JOHN = new CustomerBean(
+		(String) TEST_DATA_VARIOUS_TYPES[0][0],
+		(String) TEST_DATA_VARIOUS_TYPES[0][1],
+		(String) TEST_DATA_VARIOUS_TYPES[0][2],
+		(Date)   TEST_DATA_VARIOUS_TYPES[0][3],
+		(String) TEST_DATA_VARIOUS_TYPES[0][4],
+		(Boolean)TEST_DATA_VARIOUS_TYPES[0][5],
+		(Integer)TEST_DATA_VARIOUS_TYPES[0][6],
+		(String) TEST_DATA_VARIOUS_TYPES[0][7],
+		(String) TEST_DATA_VARIOUS_TYPES[0][8],
+		(Long)   TEST_DATA_VARIOUS_TYPES[0][9]
+		);
 	public static final CustomerStringBean JOHN_STRING = new CustomerStringBean(JOHN);
 	
 	public static final String BOB_CSV = "2,Bob,Down,25/02/1919,\"1601 Willow Rd.\r\nMenlo Park, CA 94025\r\nUnited States\","
 		+ "Y,0,\"\"\"Frankly, my dear, I don't give a damn.\"\" - Gone With The Wind\",bobdown@hotmail.com,123456";
-	public static final CustomerBean BOB = new CustomerBean("2", "Bob", "Down", date(1919, 2, 25),
-		"1601 Willow Rd.\nMenlo Park, CA 94025\nUnited States", true, 0,
-		"\"Frankly, my dear, I don't give a damn.\" - Gone With The Wind", "bobdown@hotmail.com", 123456L);
+	public static final CustomerBean BOB = new CustomerBean(
+		(String) TEST_DATA_VARIOUS_TYPES[1][0],
+		(String) TEST_DATA_VARIOUS_TYPES[1][1],
+		(String) TEST_DATA_VARIOUS_TYPES[1][2],
+		(Date)   TEST_DATA_VARIOUS_TYPES[1][3],
+		(String) TEST_DATA_VARIOUS_TYPES[1][4],
+		(Boolean)TEST_DATA_VARIOUS_TYPES[1][5],
+		(Integer)TEST_DATA_VARIOUS_TYPES[1][6],
+		(String) TEST_DATA_VARIOUS_TYPES[1][7],
+		(String) TEST_DATA_VARIOUS_TYPES[1][8],
+		(Long)   TEST_DATA_VARIOUS_TYPES[1][9]
+		);
 	public static final CustomerStringBean BOB_STRING = new CustomerStringBean(BOB);
 	
 	public static final String ALICE_CSV = "3,Alice,Wunderland,08/08/1985,\"One Microsoft Way\r\nRedmond, WA 98052-6399\r\nUnited States\","
 		+ "Y,0,\"\"\"Play it, Sam. Play \"\"As Time Goes By.\"\"\"\" - Casablanca\",throughthelookingglass@yahoo.com,2255887799";
-	public static final CustomerBean ALICE = new CustomerBean("3", "Alice", "Wunderland", date(1985, 8, 8),
-		"One Microsoft Way\nRedmond, WA 98052-6399\nUnited States", true, 0,
-		"\"Play it, Sam. Play \"As Time Goes By.\"\" - Casablanca", "throughthelookingglass@yahoo.com", 2255887799L);
+	public static final CustomerBean ALICE = new CustomerBean(
+		(String) TEST_DATA_VARIOUS_TYPES[2][0],
+		(String) TEST_DATA_VARIOUS_TYPES[2][1],
+		(String) TEST_DATA_VARIOUS_TYPES[2][2],
+		(Date)   TEST_DATA_VARIOUS_TYPES[2][3],
+		(String) TEST_DATA_VARIOUS_TYPES[2][4],
+		(Boolean)TEST_DATA_VARIOUS_TYPES[2][5],
+		(Integer)TEST_DATA_VARIOUS_TYPES[2][6],
+		(String) TEST_DATA_VARIOUS_TYPES[2][7],
+		(String) TEST_DATA_VARIOUS_TYPES[2][8],
+		(Long)   TEST_DATA_VARIOUS_TYPES[2][9]
+		);
 	public static final CustomerStringBean ALICE_STRING = new CustomerStringBean(ALICE);
 	
 	public static final String BILL_CSV = "4,Bill,Jobs,10/07/1973,\"2701 San Tomas Expressway\r\nSanta Clara, CA 95050\r\nUnited States\","
 		+ "Y,3,\"\"\"You've got to ask yourself one question: \"\"Do I feel lucky?\"\" Well, do ya, punk?\"\" - Dirty Harry\",billy34@hotmail.com,36";
-	public static final CustomerBean BILL = new CustomerBean("4", "Bill", "Jobs", date(1973, 7, 10),
-		"2701 San Tomas Expressway\nSanta Clara, CA 95050\nUnited States", true, 3,
-		"\"You've got to ask yourself one question: \"Do I feel lucky?\" Well, do ya, punk?\" - Dirty Harry",
-		"billy34@hotmail.com", 36L);
+	public static final CustomerBean BILL = new CustomerBean(
+		(String) TEST_DATA_VARIOUS_TYPES[3][0],
+		(String) TEST_DATA_VARIOUS_TYPES[3][1],
+		(String) TEST_DATA_VARIOUS_TYPES[3][2],
+		(Date)   TEST_DATA_VARIOUS_TYPES[3][3],
+		(String) TEST_DATA_VARIOUS_TYPES[3][4],
+		(Boolean)TEST_DATA_VARIOUS_TYPES[3][5],
+		(Integer)TEST_DATA_VARIOUS_TYPES[3][6],
+		(String) TEST_DATA_VARIOUS_TYPES[3][7],
+		(String) TEST_DATA_VARIOUS_TYPES[3][8],
+		(Long)   TEST_DATA_VARIOUS_TYPES[3][9]
+		);
 	public static final CustomerStringBean BILL_STRING = new CustomerStringBean(BILL);
 	
 	public static final String MIRANDA_CSV = "5,Miranda,Feist,03/01/1999,\"2-4 Rue du Sablon\r\nMorges, 1110\r\nSwitzerland\","
 		+ ",,\"\"\"You had me at \"\"hello.\"\"\"\" - Jerry Maguire\",miranda_feist@gmail.com,54623";
-	public static final CustomerBean MIRANDA = new CustomerBean("5", "Miranda", "Feist", date(1999, 1, 3),
-		"2-4 Rue du Sablon\nMorges, 1110\nSwitzerland", null, null, "\"You had me at \"hello.\"\" - Jerry Maguire",
-		"miranda_feist@gmail.com", 54623L);
+	public static final CustomerBean MIRANDA = new CustomerBean(
+		(String) TEST_DATA_VARIOUS_TYPES[4][0],
+		(String) TEST_DATA_VARIOUS_TYPES[4][1],
+		(String) TEST_DATA_VARIOUS_TYPES[4][2],
+		(Date)   TEST_DATA_VARIOUS_TYPES[4][3],
+		(String) TEST_DATA_VARIOUS_TYPES[4][4],
+		(Boolean)TEST_DATA_VARIOUS_TYPES[4][5],
+		(Integer)TEST_DATA_VARIOUS_TYPES[4][6],
+		(String) TEST_DATA_VARIOUS_TYPES[4][7],
+		(String) TEST_DATA_VARIOUS_TYPES[4][8],
+		(Long)   TEST_DATA_VARIOUS_TYPES[4][9]
+		);
 	public static final CustomerStringBean MIRANDA_STRING = new CustomerStringBean(MIRANDA);
 	
 	public static final String STEVE_CSV = "6,Steve,Gates,31/12/2000,\"701 First Avenue\r\nSunnyvale, CA 94089\r\nUnited States\","
 		+ "N,0,\"\"\"Gentlemen, you can't fight in here! This is the War Room!\"\" - Dr Strangelove\",stevengates@yahoo.com,1341512";
-	public static final CustomerBean STEVE = new CustomerBean("6", "Steve", "Gates", date(2000, 12, 31),
-		"701 First Avenue\nSunnyvale, CA 94089\nUnited States", false, 0,
-		"\"Gentlemen, you can't fight in here! This is the War Room!\" - Dr Strangelove", "stevengates@yahoo.com",
-		1341512L);
+	public static final CustomerBean STEVE = new CustomerBean(
+		(String) TEST_DATA_VARIOUS_TYPES[5][0],
+		(String) TEST_DATA_VARIOUS_TYPES[5][1],
+		(String) TEST_DATA_VARIOUS_TYPES[5][2],
+		(Date)   TEST_DATA_VARIOUS_TYPES[5][3],
+		(String) TEST_DATA_VARIOUS_TYPES[5][4],
+		(Boolean)TEST_DATA_VARIOUS_TYPES[5][5],
+		(Integer)TEST_DATA_VARIOUS_TYPES[5][6],
+		(String) TEST_DATA_VARIOUS_TYPES[5][7],
+		(String) TEST_DATA_VARIOUS_TYPES[5][8],
+		(Long)   TEST_DATA_VARIOUS_TYPES[5][9]
+		);
 	public static final CustomerStringBean STEVE_STRING = new CustomerStringBean(STEVE);
 	
 	public static final String ADA_CSV = "7,Ada,Von Trappe,18/11/1956,\"One Dell Way\r\nRound Rock, TX 78682\r\nUnited States\","
 		+ ",2,\"\"\"Hasta la vista, baby.\"\" - Terminator 2: Judgement Day\",vonada@gmail.com,0";
-	public static final CustomerBean ADA = new CustomerBean("7", "Ada", "Von Trappe", date(1956, 11, 18),
-		"One Dell Way\nRound Rock, TX 78682\nUnited States", null, 2,
-		"\"Hasta la vista, baby.\" - Terminator 2: Judgement Day", "vonada@gmail.com", 0L);
+	public static final CustomerBean ADA = new CustomerBean(
+		(String) TEST_DATA_VARIOUS_TYPES[6][0],
+		(String) TEST_DATA_VARIOUS_TYPES[6][1],
+		(String) TEST_DATA_VARIOUS_TYPES[6][2],
+		(Date)   TEST_DATA_VARIOUS_TYPES[6][3],
+		(String) TEST_DATA_VARIOUS_TYPES[6][4],
+		(Boolean)TEST_DATA_VARIOUS_TYPES[6][5],
+		(Integer)TEST_DATA_VARIOUS_TYPES[6][6],
+		(String) TEST_DATA_VARIOUS_TYPES[6][7],
+		(String) TEST_DATA_VARIOUS_TYPES[6][8],
+		(Long)   TEST_DATA_VARIOUS_TYPES[6][9]
+		);
 	public static final CustomerStringBean ADA_STRING = new CustomerStringBean(ADA);
 	
 	public static final String SERGEI_CSV = "8,Sergei,Denisovich,22/06/1944,\"1 Infinite Loop\r\nCupertino, CA 95014\r\nUnited States\","
 		+ "Y,,\"\"\"Open the pod bay doors, HAL.\"\" - 2001: A Space Odyssey\",sergei@denisovich.com,229431";
-	public static final CustomerBean SERGEI = new CustomerBean("8", "Sergei", "Denisovich", date(1944, 6, 22),
-		"1 Infinite Loop\nCupertino, CA 95014\nUnited States", true, null,
-		"\"Open the pod bay doors, HAL.\" - 2001: A Space Odyssey", "sergei@denisovich.com", 229431L);
+	public static final CustomerBean SERGEI = new CustomerBean(
+		(String) TEST_DATA_VARIOUS_TYPES[7][0],
+		(String) TEST_DATA_VARIOUS_TYPES[7][1],
+		(String) TEST_DATA_VARIOUS_TYPES[7][2],
+		(Date)   TEST_DATA_VARIOUS_TYPES[7][3],
+		(String) TEST_DATA_VARIOUS_TYPES[7][4],
+		(Boolean)TEST_DATA_VARIOUS_TYPES[7][5],
+		(Integer)TEST_DATA_VARIOUS_TYPES[7][6],
+		(String) TEST_DATA_VARIOUS_TYPES[7][7],
+		(String) TEST_DATA_VARIOUS_TYPES[7][8],
+		(Long)   TEST_DATA_VARIOUS_TYPES[7][9]
+		);
 	public static final CustomerStringBean SERGEI_STRING = new CustomerStringBean(SERGEI);
 	
 	public static final String LARRY_CSV = "9,Larry,Ballmer,01/01/1901,\"1-7-1 Konan\r\nMinato-ku\r\nTokyo, 108-0075\r\nJapan\","
 		+ "N,0,\"\"\"A martini. Shaken, not stirred.\"\" - Goldfinger\",lazza99@gmail.com,164";
-	public static final CustomerBean LARRY = new CustomerBean("9", "Larry", "Ballmer", date(1901, 1, 1),
-		"1-7-1 Konan\nMinato-ku\nTokyo, 108-0075\nJapan", false, 0, "\"A martini. Shaken, not stirred.\" - Goldfinger",
-		"lazza99@gmail.com", 164L);
+	public static final CustomerBean LARRY = new CustomerBean(
+		(String) TEST_DATA_VARIOUS_TYPES[8][0],
+		(String) TEST_DATA_VARIOUS_TYPES[8][1],
+		(String) TEST_DATA_VARIOUS_TYPES[8][2],
+		(Date)   TEST_DATA_VARIOUS_TYPES[8][3],
+		(String) TEST_DATA_VARIOUS_TYPES[8][4],
+		(Boolean)TEST_DATA_VARIOUS_TYPES[8][5],
+		(Integer)TEST_DATA_VARIOUS_TYPES[8][6],
+		(String) TEST_DATA_VARIOUS_TYPES[8][7],
+		(String) TEST_DATA_VARIOUS_TYPES[8][8],
+		(Long)   TEST_DATA_VARIOUS_TYPES[8][9]
+		);
 	public static final CustomerStringBean LARRY_STRING = new CustomerStringBean(LARRY);
 	
 	public static final String GRACE_CSV = "10,Grace,Fowler,28/11/2003,\"11-1, Kamitoba Hokotate-cho\r\nMinami-ku\r\nKyoto, 601-8501\r\nJapan\","
 		+ "N,0,\"\"\"Carpe diem. Seize the day, boys. Make your lives extraordinary.\"\" - Dead Poets Society\",gracie@hotmail.com,168841";
-	public static final CustomerBean GRACE = new CustomerBean("10", "Grace", "Fowler", date(2003, 11, 28),
-		"11-1, Kamitoba Hokotate-cho\nMinami-ku\nKyoto, 601-8501\nJapan", false, 0,
-		"\"Carpe diem. Seize the day, boys. Make your lives extraordinary.\" - Dead Poets Society",
-		"gracie@hotmail.com", 168841L);
+	public static final CustomerBean GRACE = new CustomerBean(
+		(String) TEST_DATA_VARIOUS_TYPES[9][0],
+		(String) TEST_DATA_VARIOUS_TYPES[9][1],
+		(String) TEST_DATA_VARIOUS_TYPES[9][2],
+		(Date)   TEST_DATA_VARIOUS_TYPES[9][3],
+		(String) TEST_DATA_VARIOUS_TYPES[9][4],
+		(Boolean)TEST_DATA_VARIOUS_TYPES[9][5],
+		(Integer)TEST_DATA_VARIOUS_TYPES[9][6],
+		(String) TEST_DATA_VARIOUS_TYPES[9][7],
+		(String) TEST_DATA_VARIOUS_TYPES[9][8],
+		(Long)   TEST_DATA_VARIOUS_TYPES[9][9]
+		);
 	public static final CustomerStringBean GRACE_STRING = new CustomerStringBean(GRACE);
 	
 	/**
--- super-csv/src/test/java/org/supercsv/io/CsvResultSetWriterTest.java
+++ super-csv/src/test/java/org/supercsv/io/CsvResultSetWriterTest.java
@@ -19,8 +19,9 @@ package org.supercsv.io;
 import static org.junit.Assert.*;
 import static org.supercsv.SuperCsvTestUtils.CSV_FILE;
 import static org.supercsv.SuperCsvTestUtils.HEADER;
-import static org.supercsv.SuperCsvTestUtils.STRING_CUSTOMERS;
 import static org.supercsv.SuperCsvTestUtils.WRITE_PROCESSORS;
+import static org.supercsv.SuperCsvTestUtils.TEST_DATA_VARIOUS_TYPES;
+import static org.supercsv.SuperCsvTestUtils.TEST_DATA_STRINGS;
 import static org.supercsv.SuperCsvTestUtils.date;
 
 import java.io.IOException;
@@ -34,9 +35,6 @@ import org.junit.Test;
 import org.supercsv.cellprocessor.FmtDate;
 import org.supercsv.cellprocessor.ift.CellProcessor;
 import org.supercsv.exception.SuperCsvCellProcessorException;
-import org.supercsv.exception.SuperCsvConstraintViolationException;
-import org.supercsv.mock.CustomerBean;
-import org.supercsv.mock.CustomerStringBean;
 import org.supercsv.mock.ResultSetMock;
 import org.supercsv.prefs.CsvPreference;
 
@@ -50,82 +48,6 @@ public class CsvResultSetWriterTest {
 	
 	private static final CsvPreference PREFS = CsvPreference.STANDARD_PREFERENCE;
 	
-	private static final String[] HEADERS = {
-		"customerNo", "firstName", "lastName", "birthDate",
-		"mailingAddress", "married", "numberOfKids", "favouriteQuote", 
-		"email", "loyaltyPoints" };
-	
-	public static final Object[][] TEST_DATA_VARIOUS_TYPES = {
-		{"1", "John", "Dunbar", date(1945, 6, 13),
-			"1600 Amphitheatre Parkway\nMountain View, CA 94043\nUnited States", null, null,
-			"\"May the Force be with you.\" - Star Wars", "jdunbar@gmail.com", 0L},
-		{"2", "Bob", "Down", date(1919, 2, 25),
-			"1601 Willow Rd.\nMenlo Park, CA 94025\nUnited States", true, 0,
-			"\"Frankly, my dear, I don't give a damn.\" - Gone With The Wind", "bobdown@hotmail.com", 123456L},
-		{"3", "Alice", "Wunderland", date(1985, 8, 8),
-			"One Microsoft Way\nRedmond, WA 98052-6399\nUnited States", true, 0,
-			"\"Play it, Sam. Play \"As Time Goes By.\"\" - Casablanca", "throughthelookingglass@yahoo.com", 2255887799L},
-		{"4", "Bill", "Jobs", date(1973, 7, 10),
-			"2701 San Tomas Expressway\nSanta Clara, CA 95050\nUnited States", true, 3,
-			"\"You've got to ask yourself one question: \"Do I feel lucky?\" Well, do ya, punk?\" - Dirty Harry",
-			"billy34@hotmail.com", 36L},
-		{"5", "Miranda", "Feist", date(1999, 1, 3),
-			"2-4 Rue du Sablon\nMorges, 1110\nSwitzerland", null, null, "\"You had me at \"hello.\"\" - Jerry Maguire",
-			"miranda_feist@gmail.com", 54623L},
-		{"6", "Steve", "Gates", date(2000, 12, 31),
-			"701 First Avenue\nSunnyvale, CA 94089\nUnited States", false, 0,
-			"\"Gentlemen, you can't fight in here! This is the War Room!\" - Dr Strangelove", "stevengates@yahoo.com",
-			1341512L},
-		{"7", "Ada", "Von Trappe", date(1956, 11, 18),
-			"One Dell Way\nRound Rock, TX 78682\nUnited States", null, 2,
-			"\"Hasta la vista, baby.\" - Terminator 2: Judgement Day", "vonada@gmail.com", 0L},
-		{"8", "Sergei", "Denisovich", date(1944, 6, 22),
-			"1 Infinite Loop\nCupertino, CA 95014\nUnited States", true, null,
-			"\"Open the pod bay doors, HAL.\" - 2001: A Space Odyssey", "sergei@denisovich.com", 229431L},
-		{"9", "Larry", "Ballmer", date(1901, 1, 1),
-			"1-7-1 Konan\nMinato-ku\nTokyo, 108-0075\nJapan", false, 0, "\"A martini. Shaken, not stirred.\" - Goldfinger",
-			"lazza99@gmail.com", 164L},
-		{"10", "Grace", "Fowler", date(2003, 11, 28),
-			"11-1, Kamitoba Hokotate-cho\nMinami-ku\nKyoto, 601-8501\nJapan", false, 0,
-			"\"Carpe diem. Seize the day, boys. Make your lives extraordinary.\" - Dead Poets Society",
-			"gracie@hotmail.com", 168841L},
-	};
-		
-	public static final Object[][] TEST_DATA_STRINGS = {
-		{"1", "John", "Dunbar", "13/06/1945",
-			"1600 Amphitheatre Parkway\nMountain View, CA 94043\nUnited States", "", "",
-			"\"May the Force be with you.\" - Star Wars", "jdunbar@gmail.com", "0"},
-		{"2", "Bob", "Down", "25/02/1919",
-			"1601 Willow Rd.\nMenlo Park, CA 94025\nUnited States", "Y", "0",
-			"\"Frankly, my dear, I don't give a damn.\" - Gone With The Wind", "bobdown@hotmail.com", "123456"},
-		{"3", "Alice", "Wunderland", "08/08/1985",
-			"One Microsoft Way\nRedmond, WA 98052-6399\nUnited States", "Y", "0",
-			"\"Play it, Sam. Play \"As Time Goes By.\"\" - Casablanca", "throughthelookingglass@yahoo.com", "2255887799"},
-		{"4", "Bill", "Jobs", "10/07/1973",
-			"2701 San Tomas Expressway\nSanta Clara, CA 95050\nUnited States", "Y", "3",
-			"\"You've got to ask yourself one question: \"Do I feel lucky?\" Well, do ya, punk?\" - Dirty Harry",
-			"billy34@hotmail.com", "36"},
-		{"5", "Miranda", "Feist", "03/01/1999",
-			"2-4 Rue du Sablon\nMorges, 1110\nSwitzerland", "", "", "\"You had me at \"hello.\"\" - Jerry Maguire",
-			"miranda_feist@gmail.com", "54623"},
-		{"6", "Steve", "Gates", "31/12/2000",
-			"701 First Avenue\nSunnyvale, CA 94089\nUnited States", "N", "0",
-			"\"Gentlemen, you can't fight in here! This is the War Room!\" - Dr Strangelove", "stevengates@yahoo.com",
-			"1341512"},
-		{"7", "Ada", "Von Trappe", "18/11/1956",
-			"One Dell Way\nRound Rock, TX 78682\nUnited States", "", "2",
-			"\"Hasta la vista, baby.\" - Terminator 2: Judgement Day", "vonada@gmail.com", "0"},
-		{"8", "Sergei", "Denisovich", "22/06/1944",
-			"1 Infinite Loop\nCupertino, CA 95014\nUnited States", "Y", "",
-			"\"Open the pod bay doors, HAL.\" - 2001: A Space Odyssey", "sergei@denisovich.com", "229431"},
-		{"9", "Larry", "Ballmer", "01/01/1901",
-			"1-7-1 Konan\nMinato-ku\nTokyo, 108-0075\nJapan", "N", "0", "\"A martini. Shaken, not stirred.\" - Goldfinger",
-			"lazza99@gmail.com", "164"},
-		{"10", "Grace", "Fowler", "28/11/2003",
-			"11-1, Kamitoba Hokotate-cho\nMinami-ku\nKyoto, 601-8501\nJapan", "N", "0",
-			"\"Carpe diem. Seize the day, boys. Make your lives extraordinary.\" - Dead Poets Society",
-			"gracie@hotmail.com", "168841"},
-	};
 	
 	private Writer writer;
 	private CsvResultSetWriter csvResultSetWriter;
@@ -142,7 +64,7 @@ public class CsvResultSetWriterTest {
 	 */
 	@Test
 	public void testWrite() throws IOException, SQLException {
-		ResultSet resultSetMock = new ResultSetMock(TEST_DATA_STRINGS, HEADERS);
+		ResultSet resultSetMock = new ResultSetMock(TEST_DATA_STRINGS, HEADER);
 		csvResultSetWriter.write(resultSetMock);
 		csvResultSetWriter.flush();
 		assertEquals(CSV_FILE, writer.toString());
@@ -155,7 +77,7 @@ public class CsvResultSetWriterTest {
 	 */
 	@Test
 	public void testWriteWithProcessors() throws SQLException, IOException {
-		ResultSet resultSetMock = new ResultSetMock(TEST_DATA_VARIOUS_TYPES, HEADERS);
+		ResultSet resultSetMock = new ResultSetMock(TEST_DATA_VARIOUS_TYPES, HEADER);
 		csvResultSetWriter.write(resultSetMock, WRITE_PROCESSORS);
 		csvResultSetWriter.flush();
 		assertEquals(CSV_FILE, writer.toString());
@@ -208,7 +130,7 @@ public class CsvResultSetWriterTest {
 	 */
 	@Test(expected = NullPointerException.class)
 	public void testWriteNullProcessors() throws SQLException, IOException {
-		ResultSet resultSet = new ResultSetMock(TEST_DATA_VARIOUS_TYPES, HEADERS);
+		ResultSet resultSet = new ResultSetMock(TEST_DATA_VARIOUS_TYPES, HEADER);
 		csvResultSetWriter.write(resultSet, null);
 	}
 
