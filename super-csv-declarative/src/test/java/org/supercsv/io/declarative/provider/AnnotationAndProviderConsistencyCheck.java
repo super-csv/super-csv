8b8b05cd282705fffc01a0dbf9e6d13ace6647b8
Dominik Schlosser <dominik.schlosser@gmail.com>
2016-05-14 18:22:37 +0200

    Use Parameterized test for CellProcessorProvider<->Annotation consistency check tests

--- super-csv-declarative/src/test/java/org/supercsv/io/declarative/provider/AnnotationAndProviderConsistencyCheck.java
+++ super-csv-declarative/src/test/java/org/supercsv/io/declarative/provider/AnnotationAndProviderConsistencyCheck.java
@@ -1,26 +1,37 @@
 package org.supercsv.io.declarative.provider;
 
-import java.util.Set;
+import java.util.Collection;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import org.reflections.Reflections;
 import org.supercsv.io.declarative.CellProcessor;
 
+@RunWith(Parameterized.class)
 public class AnnotationAndProviderConsistencyCheck {
+	@Parameters
+	public static Collection<Class<?>> getCellProcessorAnnotations() {
+		Reflections reflections = new Reflections("org.supercsv");
+		return reflections.getTypesAnnotatedWith(CellProcessor.class);
+	}
+	
+	private Class<?> cellProcessorAnnotationType;
+	
+	public AnnotationAndProviderConsistencyCheck(Class<?> cellProcessorAnnotationType) {
+		this.cellProcessorAnnotationType = cellProcessorAnnotationType;
+	}
+	
+	@SuppressWarnings("rawtypes")
 	@Test
 	public void test() throws InstantiationException, IllegalAccessException {
-		Reflections reflections = new Reflections("org.supercsv");
-		Set<Class<?>> cellProcessorAnnotations = reflections.getTypesAnnotatedWith(CellProcessor.class);
+		CellProcessor cellProcessorAnnotation = cellProcessorAnnotationType.getAnnotation(CellProcessor.class);
+		Class<? extends CellProcessorProvider> providerType = cellProcessorAnnotation.provider();
 		
-		for( Class<?> annotationType : cellProcessorAnnotations ) {
-			CellProcessor cellProcessorAnnotation = annotationType.getAnnotation(CellProcessor.class);
-			Class<? extends CellProcessorProvider> providerType = cellProcessorAnnotation.provider();
-			
-			CellProcessorProvider provider = providerType.newInstance();
-			
-			Assert.assertEquals(annotationType, provider.getType());
-			
-		}
+		CellProcessorProvider provider = providerType.newInstance();
+		
+		Assert.assertEquals(cellProcessorAnnotationType, provider.getType());
 	}
 }
